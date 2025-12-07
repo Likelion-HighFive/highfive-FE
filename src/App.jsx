072c2ad440c159d17ab77f7393dc3cbe71ef4e35
xeeone <xeeone@Jiwon-MacBook-Pro.local>
2025-12-07 13:53:15 +0900

    feat:signup생성

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import "./App.css";
 import Login from "./pages/auth/Login.jsx";
 import HomePage from './pages/home/HomePage';
 import BackgroundPage from './pages/background/BackgroundPage';
+import Signup from './pages/auth/Signup.jsx';
 
 export default function App() {
   return (
@@ -12,6 +13,7 @@ export default function App() {
       <Route path="/" element={<Login />} />
       <Route path="/home" element={<HomePage />} />
       <Route path="/background" element={<BackgroundPage />} />
+      <Route path="/signup" element={<Signup />} />
     </Routes>
   );
 }
