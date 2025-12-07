413f1b12e4b6f8aa5e5a66f6cea24a3dcae195c6
jeongkyueun <kyueun@swu.ac.kr>
2025-12-08 03:15:31 +0900

    feat: 좋아요 페이지 UI 완성

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import "./App.css";
 import Login from "./pages/auth/Login.jsx";
 import HomePage from './pages/home/HomePage';
 import BackgroundPage from './pages/background/BackgroundPage';
+import LikesPage from './pages/likes/LikesPage';
 import MyPage from './pages/user/MyPage';
 import Signup from './pages/auth/Signup.jsx';
 import MyCarbonFootprint from './pages/footprint/MyCarbonFootprint';
@@ -16,6 +17,7 @@ export default function App() {
       <Route path="/home" element={<HomePage />} />
       <Route path="/background" element={<BackgroundPage />} />
       <Route path="/mypage" element={<MyPage />} />
+      <Route path="/likes" element={<LikesPage />} />
       <Route path="/signup" element={<Signup />} />
       <Route path="/footprint" element={<MyCarbonFootprint />} />
     </Routes>
