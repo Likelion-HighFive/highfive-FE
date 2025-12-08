b1e4b7da8a8251c540c008268651bf934a8d2e4c
xeeone <xeeone@Jiwon-MacBook-Pro.local>
2025-12-09 07:19:31 +0900

    feat:네비게이션 시작페이지 생성

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import Login from "./pages/auth/Login.jsx";
 import HomePage from './pages/home/HomePage';
 import BackgroundPage from './pages/background/BackgroundPage';
 import Signup from './pages/auth/Signup.jsx';
+import NavigationStart from "./pages/theme/NavigationStart.jsx";
 
 export default function App() {
   return (
@@ -14,6 +15,7 @@ export default function App() {
       <Route path="/home" element={<HomePage />} />
       <Route path="/background" element={<BackgroundPage />} />
       <Route path="/signup" element={<Signup />} />
+      <Route path="/navigation" element={<NavigationStart />} />
     </Routes>
   );
 }
