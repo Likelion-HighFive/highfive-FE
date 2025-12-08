d82cf7003865104f5aa341d277ae889f7028bdb6
jeongkyueun <kyueun@swu.ac.kr>
2025-12-09 01:14:03 +0900

    feat: 플로팅 버튼 - 길추가 페이지 ui 완성

--- src/components/common/FloatingActionButtons.jsx
+++ src/components/common/FloatingActionButtons.jsx
@@ -4,15 +4,23 @@ import styles from './FloatingActionButtons.module.css';
 import plusButton from '../../assets/icons/plus_button.svg';
 import walkButton from '../../assets/icons/walk_button.svg';
 
-const FloatingActionButtons = ({ onAddPath, stepCount = 0, isHome = false }) => {
+const FloatingActionButtons = ({ stepCount = 0, isHome = false }) => {
   const navigate = useNavigate();
   
   const handleShowSteps = () => {
     navigate('/footprint');
   };
+
+  const handleAddPath = () => {
+    navigate('/create-path');
+  };
+
   return (
     <div className={styles.fabContainer}>
-      <button className={`${styles.fab} ${styles.primary}`} onClick={onAddPath}>
+      <button 
+        className={`${styles.fab} ${styles.primary}`} 
+        onClick={handleAddPath}
+      >
         <img src={plusButton} alt="경로 추가" />
       </button>
       <div className={styles.walkButtonContainer}>
